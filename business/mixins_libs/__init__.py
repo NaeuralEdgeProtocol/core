2207f91edefbbe6052d9d2b0bd9c9a3a9dd217b0
Stefan Saraev <stefan.saraev@yahoo.com>
2024-04-09 09:34:17 +0300

    fix(mixins): removed old imports, added poseapi_mixin

--- business/mixins_libs/__init__.py
+++ business/mixins_libs/__init__.py
@@ -1,12 +1,5 @@
-from .blur_movie_mixin import _BlurMovieMixin
 from .centroid_object_tracker_mixin import _CentroidObjectTrackerMixin
 from .alert_tracker_mixin import _AlertTrackerMixin
-from .pose_alert_mixin import _PoseAlertMixin
-from .pose_heuristics_mixin import _PoseHeuristicsMixin
 from .alert_witness_mixin import _AlertWitnessMixin
-from .camera_angle_mixin import _CameraAngleMixin
-from .camera_obstruction_mixin import _CameraObstructionMixin
 from .time_bins_mixin import _TimeBinsMixin
-from .lp_utils_mixin import _LpUtilsMixin  # TODO: This kind of stupid error is below middle-school guys :(
-from .lpr_sender_mixin import _LprSenderMixin
 from .poseapi_mixin import _PoseAPIMixin
