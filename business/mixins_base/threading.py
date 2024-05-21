40ec5bb6298fd9b4040554eb3929749dc11a9ee1
Stefan Saraev <stefan.saraev@yahoo.com>
2024-05-21 17:20:54 +0300

    feat(threadapi): added util functions for threadapi
    -> added threadapi_run, threadapi_map, threadapi_base64_code_map

--- business/mixins_base/threading.py
+++ business/mixins_base/threading.py
@@ -1,4 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 import os
 
 
@@ -7,15 +8,61 @@ class _ThreadingAPIMixin():
     super(_ThreadingAPIMixin, self).__init__()
     return
   
-  def threading_compute_in_parallel(self, func, lst_data, n_workers=None):
-    if n_workers is None:
-      n_workers = os.cpu_count() // 4
-    with ThreadPoolExecutor(max_workers=n_workers) as executor:
+  def __get_n_threads(self, n_threads):
+    if n_threads is None:
+      n_threads = 1
+    if n_threads > os.cpu_count() // 4:
+      self.P("Warning: n_threads is too high, setting to 1/4 of available CPUs", color='y')
+      n_threads = os.cpu_count() // 4
+    return n_threads
+  
+  def __wrapper_func_run(self, func: callable, lst_results: list, thread_id: int, n_threads: int):
+    lst_results[thread_id] = func(thread_id, n_threads)
+    return
+
+  def threadapi_map(self, func, lst_data, n_threads=1):
+    """
+    Run a function in parallel using ThreadPoolExecutor.map
+
+    Parameters
+    ----------
+    func : callable
+        The function to run in parallel
+    lst_data : list
+        The list of data to pass to the function
+    n_threads : int, optional
+        The number of threads to use, by default 1
+        If this number is higher than 1/4 of available CPUs, it will be set to 1/4 of available CPUs
+
+    Returns
+    -------
+    list
+        The results of the function (similar to list(map(func, lst_data)))
+    """
+    n_threads = self.__get_n_threads(n_threads)
+    with ThreadPoolExecutor(max_workers=n_threads) as executor:
       results = executor.map(func, lst_data)
     return list(results)
 
+  def threadapi_base64_code_map(self, base64_code, lst_data, n_threads=1):
+    """
+    Run a custom code method in parallel using ThreadPoolExecutor.map
 
-  def threading_custom_code_compute_in_parallel(self, base64_code, lst_data, n_workers=None):
+    Parameters
+    ----------
+    base64_code : str
+        The base64 encoded custom code method
+    lst_data : list
+        The list of data to pass to the custom code method
+    n_threads : int, optional
+        The number of threads to use, by default 1
+        If this number is higher than 1/4 of available CPUs, it will be set to 1/4 of available CPUs
+
+    Returns
+    -------
+    list
+        The results of the custom code method (similar to list(map(func, lst_data)))
+    """
     base_custom_code_method, errors, warnings = self._get_method_from_custom_code(
       str_b64code=base64_code,
       self_var='plugin',
@@ -31,5 +78,42 @@ class _ThreadingAPIMixin():
     warnings_str = "\n".join([str(w) for w in warnings])
     if len(warnings) > 0:
       self.P(f"Warnings found while getting custom code method:\n{warnings_str}", color='y')
-    
-    return self.threading_compute_in_parallel(custom_code_method, lst_data, n_workers)
+
+    return self.threadapi_map(custom_code_method, lst_data, n_threads)
+  
+  def threadapi_run(self, func, n_threads):
+    """
+    Run a function in parallel using threads
+
+    Parameters
+    ----------
+    func : callable
+        The function to run in parallel
+        This function must have the following signature: func(thread_id: int, n_threads: int)
+    n_threads : int
+        The number of threads to use, by default 1
+        If this number is higher than 1/4 of available CPUs, it will be set to 1/4 of available CPUs
+
+    Returns
+    -------
+    list
+        A list of results from the function calls, similar to [func(0, n_threads), func(1, n_threads), ... func(n_threads-1, n_threads)]
+    """
+    # create n_threads threads that run func
+    n_threads = self.__get_n_threads(n_threads)
+
+    lst_results = [None] * n_threads
+
+    lst_threads: list[Thread] = []
+
+    for thread_id in range(n_threads):
+      thread = Thread(target=self.__wrapper_func_run, args=(lst_results, thread_id, n_threads))
+      lst_threads.append(thread)
+
+    for thread in lst_threads:
+      thread.start()
+      
+    for thread in lst_threads:
+      thread.join()
+
+    return lst_results
