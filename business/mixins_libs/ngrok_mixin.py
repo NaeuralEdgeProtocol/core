ef6c2c5ea3ae1578614abc07673d36e48c1c2aa5
Stefan Saraev <stefan.saraev@yahoo.com>
2024-07-22 13:56:41 +0300

    refactor(ngrok): move ngrok setup in `_process` method
    -> this allows plugins to be created faster (no sleeping in init), and also allows the user to change the ngrok config after the plugin was created
    -> this fixes a bug when a user adds a typo in the config of ngrok, resulting in an Exception (which lead to a port allocation that was never freed)

--- business/mixins_libs/ngrok_mixin.py
+++ business/mixins_libs/ngrok_mixin.py
@@ -2,6 +2,7 @@ import subprocess
 
 __VER__ = '0.0.0.0'
 
+
 class _NgrokMixinPlugin(object):
   class NgrokCT:
     NG_TOKEN = 'EE_NGROK_AUTH_TOKEN'
@@ -18,29 +19,45 @@ class _NgrokMixinPlugin(object):
   and will be processed as part of the business plugin loop.
   """
 
-  def __init__(self, **kwargs):
+  def on_init(self):
+    self.cached_use_ngrok = self.cfg_use_ngrok
     self.ngrok_process = None
-    super(_NgrokMixinPlugin, self).__init__(**kwargs)
+
+    self.ngrok_authentication_done = False
+    self.ngrok_tunnel_started = False
     return
 
   @property
   def ng_token(self):
     return self.os_environ.get(_NgrokMixinPlugin.NgrokCT.NG_TOKEN, None)
 
-  def _authenticate_ngrok(self):
+  def _maybe_authenticate_ngrok(self):
+    if self.ngrok_authentication_done:
+      return
+
     ngrok_auth_args = ['ngrok', 'authtoken', self.ng_token]
     self.P('Trying to authenticate ngrok')
     auth_process = subprocess.Popen(ngrok_auth_args, stdout=subprocess.DEVNULL)
 
     # Wait for the process to finish and get the exit code
-    if auth_process.wait(timeout=10) != 0:
-      raise RuntimeError('Could not authenticate ngrok')
+    try:
+      if auth_process.wait(timeout=10) != 0:
+        raise RuntimeError('Could not authenticate ngrok. Please check your ngrok authentication token.')
+    except subprocess.TimeoutExpired:
+      auth_process.kill()
+      raise RuntimeError('Could not authenticate ngrok. Authentication process timed out.')
+
+    self.ngrok_authentication_done = True
     self.P('Successfully authenticated ngrok.', color='g')
     return
 
-  def _start_ngrok_tunnel(self):
+  def _maybe_start_ngrok_tunnel(self):
+    if self.ngrok_tunnel_started:
+      return
+
     # Start ngrok in the background.
-    self.P('Starting ngrok on port {} with domain {}'.format(self.port, self.cfg_ngrok_domain))
+    self.P('Starting ngrok on port {} with domain {} and edge {}'.format(
+      self.port, self.cfg_ngrok_domain, self.cfg_ngrok_edge_label))
     if self.cfg_ngrok_edge_label is not None:
       ngrok_start_args = [
         'ngrok',
@@ -57,47 +74,29 @@ class _NgrokMixinPlugin(object):
         f'--domain={self.cfg_ngrok_domain}'
       ]
     else:
-      raise RuntimeError("No domain/edge specified")
-    #endif
+      raise RuntimeError("No domain/edge specified. Please check your configuration.")
+    # endif
 
     self.ngrok_process = subprocess.Popen(ngrok_start_args, stdout=subprocess.DEVNULL)
-    #FIXME: this returns the pid, but what happens in case of error?
-    self.P('ngrok started with PID {}'.format(self.ngrok_process.pid), color='g')
-    return
+    # FIXME: this returns the pid, but what happens in case of error?
 
-  def _check_ngrok_started_successfully(self):
+    self.P('Starting ngrok process with PID {}'.format(self.ngrok_process.pid))
+
     # Wait until ngrok has started.
-    self.P('Waiting for ngrok to start...', color='b')
     self.sleep(10)
 
     # FIXME: does this assumes linux (even if we run with docker)?
     if not self.os_path.exists('/proc/' + str(self.ngrok_process.pid)):
       raise RuntimeError('Failed to start ngrok')
-    self.P('ngrok started successfully."', color='g')
+    self.ngrok_tunnel_started = True
+    self.P('ngrok started successfully.')
 
     return
 
-  def on_init(self):
-    super(_NgrokMixinPlugin, self).on_init()
-
-    if not self.cfg_use_ngrok:
+  def _maybe_close_ngrok_tunnel(self):
+    if not self.ngrok_tunnel_started:
       return
 
-    self._authenticate_ngrok()
-
-    self._start_ngrok_tunnel()
-
-    self._check_ngrok_started_successfully()
-
-    self.P('Starting app...')
-    return
-
-  def on_close(self):
-    super(_NgrokMixinPlugin, self).on_close()
-    if not self.cfg_use_ngrok:
-      return
-
-    # Teardown ngrok
     try:
       self.P('Killing ngrok..')
       self.ngrok_process.kill()
@@ -105,4 +104,28 @@ class _NgrokMixinPlugin(object):
     except Exception as _:
       self.P('Could not kill ngrok server')
 
+    # FIXME: we assume that the process is killed, even when an exception is raised.
+    self.ngrok_tunnel_started = False
+    return
+
+  def _process(self):
+    super(_NgrokMixinPlugin, self)._process()
+
+    if self.cfg_use_ngrok:
+      self._maybe_authenticate_ngrok()
+      self._maybe_start_ngrok_tunnel()
+    # endif
+    return
+
+  def on_config(self):
+    super(_NgrokMixinPlugin, self).on_config()
+    if self.cached_use_ngrok and not self.cfg_use_ngrok:
+      self.P("User disabled ngrok. Closing tunnel.")
+      self._maybe_close_ngrok_tunnel()
+    # endif
+    return
+
+  def on_close(self):
+    super(_NgrokMixinPlugin, self).on_close()
+    self._maybe_close_ngrok_tunnel()
     return
