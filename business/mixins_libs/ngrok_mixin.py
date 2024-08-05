9b62053a9cd02b7b4ac692b5d7c5132512209fa9
Stefan Saraev <stefan.saraev@yahoo.com>
2024-08-05 03:04:39 +0300

    refactor(ngrok): use run and wait process from base web app
    -> changed all non-api methods to private and moved them to the beginning of the file
    -> changed all variables to be private
    -> use run process and wait for process and log readers to catch all ngrok proc logs
    -> changed the ngrok process method to be non-blocking (the 10 sec block when starting ngrok)

--- business/mixins_libs/ngrok_mixin.py
+++ business/mixins_libs/ngrok_mixin.py
@@ -19,113 +19,182 @@ class _NgrokMixinPlugin(object):
   and will be processed as part of the business plugin loop.
   """
 
-  def on_init(self):
-    self.cached_use_ngrok = self.cfg_use_ngrok or self.cfg_ngrok_enabled
-    self.ngrok_process = None
-
-    self.ngrok_authentication_done = False
-    self.ngrok_tunnel_started = False
-    return
-
   @property
-  def ng_token(self):
+  def __ng_token(self):
     return self.os_environ.get(_NgrokMixinPlugin.NgrokCT.NG_TOKEN, None)
 
-  def _maybe_authenticate_ngrok(self):
-    if self.ngrok_authentication_done:
-      return
+  def __maybe_print_ngrok_logs(self):
+    if self.__ngrok_log_reader is not None:
+      logs = self.__ngrok_log_reader.get_next_characters()
+      if len(logs) > 0:
+        self.P(logs)
 
-    ngrok_auth_args = ['ngrok', 'authtoken', self.ng_token]
-    self.P('Trying to authenticate ngrok')
-    auth_process = subprocess.Popen(ngrok_auth_args, stdout=subprocess.DEVNULL)
-
-    # Wait for the process to finish and get the exit code
-    try:
-      if auth_process.wait(timeout=10) != 0:
-        raise RuntimeError('Could not authenticate ngrok. Please check your ngrok authentication token.')
-    except subprocess.TimeoutExpired:
-      auth_process.kill()
-      raise RuntimeError('Could not authenticate ngrok. Authentication process timed out.')
-
-    self.ngrok_authentication_done = True
-    self.P('Successfully authenticated ngrok.', color='g')
+    if self.__ngrok_error_log_reader is not None:
+      err_logs = self.__ngrok_error_log_reader.get_next_characters()
+      if len(err_logs) > 0:
+        self.P(err_logs, color='r')
     return
 
-  def _maybe_start_ngrok_tunnel(self):
-    if self.ngrok_tunnel_started:
+  def __maybe_read_and_stop_ngrok_log_readers(self):
+    if self.__ngrok_log_reader is not None:
+      self.__ngrok_log_reader.stop()
+    if self.__ngrok_error_log_reader is not None:
+      self.__ngrok_error_log_reader.stop()
+
+    self.__maybe_print_ngrok_logs()
+
+    self.__ngrok_log_reader = None
+    self.__ngrok_error_log_reader = None
+    return
+
+  def __maybe_authenticate_ngrok(self):
+    if self.__ngrok_authentication_finished:
       return
 
-    # Start ngrok in the background.
-    self.P('Starting ngrok on port {} with domain {} and edge {}'.format(
-      self.port, self.cfg_ngrok_domain, self.cfg_ngrok_edge_label))
-    if self.cfg_ngrok_edge_label is not None:
-      ngrok_start_args = [
-        'ngrok',
-        'tunnel',
-        str(self.port),
-        '--label',
-        f'edge={self.cfg_ngrok_edge_label}'
-      ]
-    elif self.cfg_ngrok_domain is not None:
-      ngrok_start_args = [
-        'ngrok',
-        'http',
-        str(self.port),
-        f'--domain={self.cfg_ngrok_domain}'
-      ]
-    else:
-      raise RuntimeError("No domain/edge specified. Please check your configuration.")
+    if not self.__ngrok_authentication_started:
+      self.__ngrok_authentication_started = True
+      ngrok_auth_args = ['ngrok', 'authtoken', self.__ng_token]
+      self.P('Trying to authenticate ngrok')
+      self.__ngrok_auth_process, self.__ngrok_log_reader, self.__ngrok_error_log_reader = self.run_command(
+        ngrok_auth_args)
+      self.__ngrok_authentication_started_time = self.time()
+
+    timeout = 0.1
+    finished, failed = self.wait_for_command(
+      self.__ngrok_auth_process,
+      timeout,
+      lst_log_reader=[self.__ngrok_log_reader, self.__ngrok_error_log_reader]
+    )
+
+    if finished and not failed:
+      self.__maybe_read_and_stop_ngrok_log_readers()
+      self.__ngrok_authentication_finished = True
+      self.P('Successfully authenticated ngrok.', color='g')
+
+    elif finished and failed:
+      self.__maybe_read_and_stop_ngrok_log_readers()
+      self.__ngrok_authentication_started = False
+      raise RuntimeError('Could not authenticate ngrok. Please check your ngrok authentication token.')
+    elif not finished:
+      if self.time() - self.__ngrok_authentication_started_time > 3:
+        self.__ngrok_auth_process.kill()
+        self.__maybe_read_and_stop_ngrok_log_readers()
+        self.__ngrok_authentication_started = False
+        raise RuntimeError('Could not authenticate ngrok. Authentication process timed out.')
+    return
+
+  def __maybe_start_ngrok_tunnel(self):
+    if self.__ngrok_tunnel_checked:
+      # Tunnel is up and ready
+      return
+
+    if not self.__ngrok_authentication_finished:
+      # Wait for authentication to finish before starting the tunnel.
+      return
+
+    if not self.__ngrok_tunnel_started:
+      self.__ngrok_tunnel_started = True
+
+      # Start ngrok in the background.
+      self.P('Starting ngrok on port {} with domain {} and edge {}'.format(
+        self.port, self.cfg_ngrok_domain, self.cfg_ngrok_edge_label))
+      if self.cfg_ngrok_edge_label is not None:
+        ngrok_start_args = [
+          'ngrok',
+          'tunnel',
+          str(self.port),
+          '--label',
+          f'edge={self.cfg_ngrok_edge_label}'
+        ]
+      elif self.cfg_ngrok_domain is not None:
+        ngrok_start_args = [
+          'ngrok',
+          'http',
+          str(self.port),
+          f'--domain={self.cfg_ngrok_domain}'
+        ]
+      else:
+        self.__ngrok_tunnel_started = False
+        raise RuntimeError("No domain/edge specified. Please check your configuration.")
+      # endif
+
+      self.__ngrok_tunnel_process, self.__ngrok_log_reader, self.__ngrok_error_log_reader = self.run_command(ngrok_start_args)
+      self.__ngrok_tunnel_started_time = self.time()
+      self.P('Starting ngrok process with PID {}'.format(self.__ngrok_tunnel_process.pid))
     # endif
 
-    self.ngrok_process = subprocess.Popen(ngrok_start_args, stdout=subprocess.DEVNULL)
-    # FIXME: this returns the pid, but what happens in case of error?
+    timeout = 0.1
+    finished, _ = self.wait_for_command(
+      self.__ngrok_tunnel_process,
+      timeout,
+      lst_log_reader=[self.__ngrok_log_reader, self.__ngrok_error_log_reader]
+    )
 
-    self.P('Starting ngrok process with PID {}'.format(self.ngrok_process.pid))
-
-    # Wait until ngrok has started.
-    self.sleep(10)
-
-    # FIXME: does this assumes linux (even if we run with docker)?
-    if not self.os_path.exists('/proc/' + str(self.ngrok_process.pid)):
-      raise RuntimeError('Failed to start ngrok')
-    self.ngrok_tunnel_started = True
-    self.P('ngrok started successfully.')
+    if finished:
+      self.__maybe_read_and_stop_ngrok_log_readers()
+      self.__ngrok_tunnel_started = False
+      raise RuntimeError('Ngrok process finished unexpectedly. Please check the logs.')
+    elif self.time() - self.__ngrok_tunnel_started_time > 3:
+      self.P('ngrok started successfully.')
+      self.__ngrok_tunnel_checked = True
 
     return
 
-  def _maybe_close_ngrok_tunnel(self):
-    if not self.ngrok_tunnel_started:
+  def __maybe_close_ngrok_tunnel(self):
+    if not self.__ngrok_tunnel_started:
       return
 
     try:
       self.P('Killing ngrok..')
-      self.ngrok_process.kill()
+      self.__ngrok_tunnel_process.kill()
+      self.__maybe_read_and_stop_ngrok_log_readers()
       self.P('Killed ngrok')
     except Exception as _:
       self.P('Could not kill ngrok server')
 
     # FIXME: we assume that the process is killed, even when an exception is raised.
-    self.ngrok_tunnel_started = False
+    self.__ngrok_tunnel_started = False
+    return
+
+  # Public API
+  def on_init(self):
+    super(_NgrokMixinPlugin, self).on_init()
+    self.__cached_use_ngrok = self.cfg_use_ngrok or self.cfg_ngrok_enabled
+    self.__ngrok_tunnel_process = None
+
+    self.__ngrok_auth_process = None
+    self.__ngrok_authentication_started = False
+    self.__ngrok_authentication_started_time = 0
+    self.__ngrok_authentication_finished = False
+
+    self.__ngrok_tunnel_process = None
+    self.__ngrok_tunnel_started = False
+    self.__ngrok_tunnel_started_time = 0
+    self.__ngrok_tunnel_checked = False
+
+    self.__ngrok_log_reader = None
+    self.__ngrok_error_log_reader = None
     return
 
   def _process(self):
     super(_NgrokMixinPlugin, self)._process()
 
     if self.cfg_use_ngrok or self.cfg_ngrok_enabled:
-      self._maybe_authenticate_ngrok()
-      self._maybe_start_ngrok_tunnel()
+      self.__maybe_authenticate_ngrok()
+      self.__maybe_start_ngrok_tunnel()
+      self.__maybe_print_ngrok_logs()
     # endif
     return
 
-  def on_config(self):
-    super(_NgrokMixinPlugin, self).on_config()
-    if self.cached_use_ngrok and not (self.cfg_use_ngrok or self.cfg_ngrok_enabled):
+  def _on_config(self):
+    super(_NgrokMixinPlugin, self)._on_config()
+    if self.__cached_use_ngrok and not (self.cfg_use_ngrok or self.cfg_ngrok_enabled):
       self.P("User disabled ngrok. Closing tunnel.")
-      self._maybe_close_ngrok_tunnel()
+      self.__maybe_close_ngrok_tunnel()
     # endif
     return
 
-  def on_close(self):
-    super(_NgrokMixinPlugin, self).on_close()
-    self._maybe_close_ngrok_tunnel()
+  def _on_close(self):
+    super(_NgrokMixinPlugin, self)._on_close()
+    self.__maybe_close_ngrok_tunnel()
     return
